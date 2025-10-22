2246e8d8dddb20ff5ab5de58799854c988cea8f1
Petyo Stoyanov <petyosv@gmail.com>
2025-10-22 15:36:13 +0300

    Fix styled-components typings and export registry by default

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "@4i4/theme-toolkit";
-export { default as registry } from "./registry";
+export { default as default, default as registry } from "./registry";
 export { default as themeDefinition } from "./theme.json";
 export { starterSettingsDefaults } from "./settings/defaults";
 export type { StarterSettings } from "./settings/defaults";
